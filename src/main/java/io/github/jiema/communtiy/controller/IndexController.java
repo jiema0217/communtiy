2d733e3318685fa8f83f89c2d8621955daf868dc
jiema0217 <924616655@qq.com>
2020-05-12 15:48:43 +0800

    add check cookies

--- src/main/java/io/github/jiema/communtiy/controller/IndexController.java
+++ src/main/java/io/github/jiema/communtiy/controller/IndexController.java
@@ -1,12 +1,33 @@
 package io.github.jiema.communtiy.controller;
 
+import io.github.jiema.communtiy.mapper.UserMapper;
+import io.github.jiema.communtiy.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Resource
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- src/main/java/io/github/jiema/communtiy/mapper/UserMapper.java
+++ src/main/java/io/github/jiema/communtiy/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package io.github.jiema.communtiy.mapper;
 import io.github.jiema.communtiy.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("INSERT INTO user(name,account_id,token,gmt_create,gmt_modified) VALUES (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * FROM  user WHERE token=#{token}")
+    User findByToken(@Param("token") String token);
 }
